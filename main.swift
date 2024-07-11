0324da01a7989bdd6af63c6dd2b0130497b1d3c3
klaurenn <kaelalhall@gmail.com>
2024-07-11 14:14:27 -0400

    add coding solution

---
+++ main.swift
@@ -0,0 +1,23 @@
+let appName = "KaeCooks"
+let developerName = "Kaela"
+let intialreleaseyear = "2024"
+
+print("My app name: \(appName)")
+print("My developer name: \(developerName)")
+print("My intial release year: \(intialreleaseyear)")
+
+var currentVersion: Double = 0.1
+var numberofdownloads: Int = 1000
+var userRating: Double = 0.2
+
+print("Current Version: \(currentVersion)")
+print("Number of Downloads: \(numberofdownloads)")
+print("User Rating: \(userRating)")
+
+currentVersion += 0.1
+numberofdownloads -= 1000
+userRating += 0.2
+
+print("Current Version: \(currentVersion)")
+print("Number of Downloads: \(numberofdownloads)")
+print("User Rating: \(userRating)")
